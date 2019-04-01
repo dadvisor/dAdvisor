f6aa961785f04d6816e38d509a0e776c90f66f47
Patrick Vogel <patrickvogel@live.nl>
2019-04-01 17:11:13 +0200

    removed too much

--- web/__init__.py
+++ web/__init__.py
@@ -5,10 +5,12 @@ from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 
 from datatypes.address import IP
+from datatypes.encoder import JSONCustomEncoder
 
 
 def create_web_app(container_thread, peers_thread, inspector_thread, analyser_thread):
     app = Flask(__name__)
+    app.json_encoder = JSONCustomEncoder
     CORS(app)
 
     log = logging.getLogger('werkzeug')
