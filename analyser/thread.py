ce28e88f1dc161a606a9215fb22d548be9cf2554
Patrick Vogel <patrickvogel@live.nl>
2019-04-03 11:44:20 +0200

    small update

--- analyser/thread.py
+++ analyser/thread.py
@@ -33,6 +33,7 @@ class AnalyserThread(Thread):
 
             if src_id == -1 or dst_id == -1:
                 continue
+            log.info(dataflow)
 
             if src_id in self.data:
                 if dst_id in self.data[src_id]:
