4c6fad55d12c10106bbb810288c3a4a6e7793973
Patrick Vogel <patrickvogel@live.nl>
2019-04-03 20:20:14 +0200

    added configuration

--- main2.py
+++
@@ -1,9 +0,0 @@
-import random
-
-from prometheus_client import start_http_server, Counter
-
-if __name__ == '__main__':
-    start_http_server(8000)
-    c = Counter('test', 'description')
-    while True:
-        c.inc(random.random())
