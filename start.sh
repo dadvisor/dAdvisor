f7fe27b14783eadf5252aa9097624ee5cfb7a015
Patrick Vogel <patrickvogel@live.nl>
2019-04-05 15:26:19 +0200

    update dockerfile

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+set -e
 
 tor &
 ./prometheus-*/prometheus &
+
+if [[ "$(stat -c "%U:%G" /grafana/data)" != grafana:grafana ]]; then
+	chown grafana:grafana /grafana/data
+fi
+
+exec su-exec grafana grafana-server --homepath=/grafana &
+
+
 python main.py
