0b767a60f86b66d20584b6e292f7e810344f4269
Patrick Vogel <patrickvogel@live.nl>
2019-04-08 17:03:38 +0200

    fixed imports

--- dadvisor/datatypes/encoder.py
+++ dadvisor/datatypes/encoder.py
@@ -4,11 +4,11 @@ from flask.json import JSONEncoder
 class JSONCustomEncoder(JSONEncoder):
 
     def default(self, obj):
-        from datatypes.address import Address
-        from datatypes.container_info import ContainerInfo
-        from datatypes.container_mapping import ContainerMapping
-        from datatypes.dataflow import DataFlow
-        from datatypes.peer import Peer
+        from ..datatypes.address import Address
+        from ..datatypes.container_info import ContainerInfo
+        from ..datatypes.container_mapping import ContainerMapping
+        from ..datatypes.dataflow import DataFlow
+        from ..datatypes.peer import Peer
         if isinstance(obj, (Peer, Address, ContainerMapping, ContainerInfo, DataFlow)):
             return obj.__dict__()
         return JSONEncoder.default(self, obj)
