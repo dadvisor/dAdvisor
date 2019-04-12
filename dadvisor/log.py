d0191fdf606e9ea014fa957c8a026083f40f1afd
Patrick Vogel <patrickvogel@live.nl>
2019-04-12 20:28:03 +0200

    renamed dadvisor to src

--- dadvisor/log.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-log = logging.getLogger()
-log.setLevel(logging.INFO)
-console = logging.StreamHandler()
-format_str = '%(asctime)s\t%(levelname)s -- %(filename)s:%(lineno)s -- %(message)s'
-console.setFormatter(logging.Formatter(format_str))
-log.addHandler(console)
