0c8923b0e7bca49bf21ba4c6016f7cc8877509ab
Patrick Vogel <patrickvogel@live.nl>
2019-04-24 15:51:03 +0200

    implemented waste

--- src/dadvisor/config.py
+++ src/dadvisor/config.py
@@ -8,6 +8,8 @@ PROXY_PORT = int(os.environ.get('NGINX_PORT', 5000))
 INTERNAL_PORT = 8800
 INTERNAL_IP = socket.gethostbyname(socket.gethostname())
 CADVISOR_URL = 'http://localhost:8080'
+PROMETHEUS_URL = 'http://localhost:{}/prometheus'.format(PROXY_PORT)
+
 IP = requests.get('https://api.ipify.org?format=json').json()['ip']
 
 INFO_HASH = os.environ.get('INFO_HASH', 'uniquetoken')
--- src/dadvisor/cost_waste/waste_collector.py
+++ src/dadvisor/cost_waste/waste_collector.py
@@ -1,7 +1,9 @@
 from prometheus_client import Counter
 
+from dadvisor.log import log
 from dadvisor.config import GB_PRICE_SECOND, gb_to_bytes, CPU_PRICE_SECOND
 from dadvisor.containers.cadvisor import get_storage_info
+from dadvisor.containers.prometheus import get_cpu_stat
 
 
 class WasteCollector(object):
@@ -11,17 +13,20 @@ class WasteCollector(object):
         self.counter = Counter('computational_waste_dollar', 'Waste in dollars of this host', ['host'])
 
     async def collect_waste(self, elapsed):
-        info = await get_storage_info()
-
-        total = self.collect_computational_waste(info['num_cores'], elapsed) + self.collect_memory_waste(info, elapsed)
+        total = await self.collect_computational_waste(elapsed) + await self.collect_memory_waste(elapsed)
         self.counter.labels(self.peers_collector.my_peer.host).inc(total)
 
     @staticmethod
-    def collect_computational_waste(num_cores, elapsed_time):
+    async def collect_computational_waste(elapsed_time):
+        data = await get_cpu_stat()
+        log.info(data)
+        num_cores = data['data']['result'][0]['value'][1]
+        log.info(num_cores)
         return CPU_PRICE_SECOND * num_cores * elapsed_time
 
     @staticmethod
-    def collect_memory_waste(info, elapsed_time):
+    async def collect_memory_waste(elapsed_time):
+        info = await get_storage_info()
         memory = sum([fs['usage'] for fs in info if fs['device'].startswith('/dev/')])
 
         return GB_PRICE_SECOND * gb_to_bytes(memory) * elapsed_time
