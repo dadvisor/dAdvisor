6c803798789071752f1cf6d67c05a865bdc1dbfb
Patrick Vogel <patrickvogel@live.nl>
2019-05-27 11:52:25 +0200

    fixed bug

--- src/dadvisor/log.py
+++ src/dadvisor/log.py
@@ -4,8 +4,6 @@ Declare some logging configuration, such that it prints pretty
 
 import logging
 
-from dadvisor.config import LOG_LEVEL
-
 log = logging.getLogger()
 console = logging.StreamHandler()
 format_str = '%(asctime)s\t%(levelname)s -- %(filename)s:%(lineno)s -- %(message)s'
