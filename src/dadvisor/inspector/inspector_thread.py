cc7c48c8953378d58c444106ca16de744c3a2959
Patrick Vogel <patrickvogel@live.nl>
2019-05-29 10:14:55 +0200

    small update

--- src/dadvisor/inspector/inspector_thread.py
+++ src/dadvisor/inspector/inspector_thread.py
@@ -31,8 +31,9 @@ class InspectorThread(Thread):
                 if dataflow.size > 0 and not self.is_p2p_communication(dataflow):
                     self.analyser.loop.create_task(
                         self.analyser.analyse_dataflow(dataflow))
-            except ValueError:
-                log.warn('Cannot parse row: %s' % row.decode('utf-8').rstrip())
+            except Exception as e:
+                log.error(e)
+                log.error('Cannot parse row: %s' % row.decode('utf-8').rstrip())
 
     @staticmethod
     def check_installation():
