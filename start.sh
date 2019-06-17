3b4fe7d5efbf2a028d779bb07ae9afb135cda33f
Patrick Vogel <patrickvogel@live.nl>
2019-06-17 13:29:32 +0200

    updated start script

--- start.sh
+++ start.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 set -ex
 
-./prometheus/prometheus --web.listen-address=:14102 \
---web.external-url http://localhost:14100/prometheus/ &
+if [[$DATA_REPLICATION='1']]; then
+	echo "Starting Prometheus and Grafana"
 
-if [[ "$(stat -c "%U:%G" /grafana/data)" != grafana:grafana ]]; then
-	chown grafana:grafana /grafana/data
+	./prometheus/prometheus --web.listen-address=:14102 \
+	--web.external-url http://localhost:14100/prometheus/ &
+
+	if [[ "$(stat -c "%U:%G" /grafana/data)" != grafana:grafana ]]; then
+		chown grafana:grafana /grafana/data
+	fi
+
+	# debug command
+	cd /grafana/plugins/containers-panel && git pull && cd /
+
+
+	su-exec grafana grafana-server --homepath=/grafana &
+else
+	echo "No prometheus and grafana: $(DATA_REPLICATION)"
 fi
 
-# debug command
-cd /grafana/plugins/containers-panel && git pull && cd /
-
 /usr/bin/cadvisor --port 14104 &
-su-exec grafana grafana-server --homepath=/grafana &
-
 nginx
 python3 -u src/main.py
